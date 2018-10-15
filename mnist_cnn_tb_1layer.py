0e17be54a0e00a0da66be2fb8b3aeb2a480244e4
cyq7on <cyq7on@qq.com>
2018-10-15 23:06:25 +0800

    Supply

--- mnist_cnn_tb_1layer.py
+++ mnist_cnn_tb_1layer.py
@@ -20,19 +20,13 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-def avg_pool_2x2(x):
-    return tf.nn.avg_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-conv_core_size = 3;
-
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print("Download Done!")
 
 sess = tf.InteractiveSession()
 
 # paras
-W_conv1 = weight_variable([conv_core_size, conv_core_size, 1, 32])
+W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
 # conv layer-1
@@ -40,20 +34,20 @@ x = tf.placeholder(tf.float32, [None, 784])
 x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = avg_pool_2x2(h_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
-# conv layer-2
-W_conv2 = weight_variable([conv_core_size, conv_core_size, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = avg_pool_2x2(h_conv2)
+# # conv layer-2
+# W_conv2 = weight_variable([5, 5, 32, 64])
+# b_conv2 = bias_variable([64])
+#
+# h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+# h_pool2 = max_pool_2x2(h_conv2)
 
 # full connection
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([14 * 14 * 32, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_pool2_flat = tf.reshape(h_pool1, [-1, 14 * 14 * 32])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 # dropout
